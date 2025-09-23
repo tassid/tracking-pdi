7508e439ef96da4a8d4d3cdd146a3655081597db
tassi <tassianeanzolin@gmail.com>
2025-09-23 16:13:09 -0400

    added readme.md and changed tracking.py

--- tracking.py
+++ tracking.py
@@ -286,16 +286,4 @@ def main():
             pass
 
 if __name__ == "__main__":
-    """
-    Requisitos:
-      - Python 3.8+
-      - opencv-python (ou opencv-contrib-python para mais trackers): pip install opencv-contrib-python
-      - (Opcional) yt-dlp para --url: pip install yt-dlp
-    Exemplo:
-      1) Baixe manualmente com yt-dlp: yt-dlp -f mp4 "URL" -o input.mp4
-         python tracking.py --video input.mp4 --out saida.mp4
-      2) Baixe automático:
-         python tracking.py --url "https://www.youtube.com/watch?v=XXXX" --out saida.mp4
-      3) Selecione múltiplas ROIs com o mouse e confirme com ENTER.
-    """
     main()
